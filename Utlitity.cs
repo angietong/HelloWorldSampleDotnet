aa667d2c717ddf15dfed7dfe413a378dcdae9161
varockiasamy <varockiasamy@bhg-inc.com>
2019-06-09 15:10:41 -0400

    Initial Commit

---
+++ Utlitity.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorldSampleDotnet
+{
+    public static class Utlitity
+    {
+        public static void WelcomeMessage()
+        {
+            Console.WriteLine("Hello World for Jenkins CI");
+            //Console.ReadLine();
+        }
+
+    }
+}
